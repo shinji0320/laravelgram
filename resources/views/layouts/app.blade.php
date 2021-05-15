7ce2d80f412684a481f5717a2794199cceb7489b
shinji0320 <shinji.blue.one@gmail.com>
2021-05-15 11:20:14 +0900

    [add]profile/edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,26 +14,14 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
+
+      @yield('navbar')
+
       <div class="container">
-        <a class="navbar__brand navbar__mainLogo" href="/">Topicon</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-md-auto align-items-center">
-            <li>
-              <a class="btn btn-primary" href="/posts/new">投稿</a>
-            </li>
-            <li>
-              <a class="nav-link commonNavIcon profile-icon" href="#">profile</a>
-            </li>
-          </ul>
-        </div>
+         @yield('content')
       </div>
-    </nav>
-       
-       @yield('content')
+      @yield('footer')
+
 
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,15 @@
 */
 
 Route::get('/', 'PostsController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//ユーザ編集画面
+Route::get('/users/edit', 'UsersController@edit');
+//ユーザ更新画面
+Route::post('/users/update', 'UsersController@update');
+
+// ユーザ詳細画面
+Route::get('/users/{user_id}', 'UsersController@show');
